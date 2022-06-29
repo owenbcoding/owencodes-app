28c55e9bef3265f065c4351fea599b8fffbd1716
eoghankbdev <eoghankb@gmail.com>
2022-06-29 22:08:18 +0100

    Added Cursor Pointer to Tech

--- src/Home.js
+++ src/Home.js
@@ -3,9 +3,9 @@ function Home() {
 
   return (
     <div className="Home absolute bottom-0 flex justify-center py-8 mx-auto mt-40 bg-[#1e1f26] w-full">
-            <p className="text-4xl text-teal-500 font-medium mx-10">React</p>
-            <p className="text-4xl text-teal-500 font-medium mx-10">TailwindCSS</p>
-            <p className="text-4xl text-teal-500 font-medium mx-10">GraphQL</p>
+            <p className="text-4xl text-teal-500 font-medium mx-10 cursor-pointer">React</p>
+            <p className="text-4xl text-teal-500 font-medium mx-10 cursor-pointer">TailwindCSS</p>
+            <p className="text-4xl text-teal-500 font-medium mx-10 cursor-pointer">GraphQL</p>
     </div>
   );
 }
