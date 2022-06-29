573be482034c368a7cd8e96bda015f580397fe2b
eoghankbdev <eoghankb@gmail.com>
2022-06-29 20:40:40 +0100

    updated home page

--- src/Home.js
+++ src/Home.js
@@ -2,9 +2,13 @@
 function Home() {
 
   return (
-    <div className="Home">
-      
-
+    <div className="Home container flex justify-around py-8 mx-auto mt-48 bg-[#1e1f26]">
+       <div className="nav-container flex space-x-8 font-medium text-teal-500">
+            <p className="text-4xl">React</p>
+            <p className="text-4xl">TailwindCSS</p>
+            <p className="text-4xl">GraphQL</p>
+            <p className="text-4xl">ThreeJS</p>
+        </div>
     </div>
   );
 }
