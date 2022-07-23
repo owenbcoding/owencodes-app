b4e8bb57e9001a9241b5c29fe473331d68a2ac00
eoghankbdev <eoghankb@gmail.com>
2022-07-23 23:56:24 +0100

    Disabled Mouse and made text smaller

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, {useCallback} from 'react'
 import Particles from 'react-tsparticles'
 import {loadFull} from 'tsparticles'
-import { useState } from "react";
+import {useState} from 'react'
 // import logo from "./logo.svg";
 import Navbar from './Navbar'
 import Home from './Home'
-import NavLink from './NavLink';
+import NavLink from './NavLink'
 import particlesOptions from './particles.json'
 
 function App() {
@@ -13,14 +13,14 @@ function App() {
 		loadFull(main)
 	}, [])
 
-  const [links, setLinks] = useState([
-    { name: 'Twitch', href: 'https://www.twitch.tv/owencodes'},
-    { name: 'Twitter', href: 'https://twitter.com/eoghanb2'},
-    { name: 'Github', href: 'https://github.com/owenbcoding'},
-    { name: 'Youtube', href: 'https://www.youtube.com/channel/UCdSxkWueHp3D5lkm0YYomaw'},
-    { name: 'Discord', href: 'https://discord.gg/XknredJKUJ'},
-    { name: 'LinkedIn', href: 'https://www.linkedin.com/in/eoghan-byrne-97221518a/'}
-  ])
+	const [links, setLinks] = useState([
+		{name: 'Twitch', href: 'https://www.twitch.tv/owencodes'},
+		{name: 'Twitter', href: 'https://twitter.com/eoghanb2'},
+		{name: 'Github', href: 'https://github.com/owenbcoding'},
+		{name: 'Youtube', href: 'https://www.youtube.com/channel/UCdSxkWueHp3D5lkm0YYomaw'},
+		{name: 'Discord', href: 'https://discord.gg/XknredJKUJ'},
+		{name: 'LinkedIn', href: 'https://www.linkedin.com/in/eoghan-byrne-97221518a/'},
+	])
 
 	return (
 		<div className='text-center overflow-hidden h-full '>
@@ -29,23 +29,15 @@ function App() {
 			<header className='flex flex-col justify-center text-white text-x1'>
 				{/* <img src={logo} className="App-logo" alt="logo"/> */}
 				<h1 className='text-6xl mt-20 text-teal-500 text-'>Owencodes</h1>
-				<p className='text-4xl text-teal-500 mt-10'>Full Stack Developer, Web3 &</p>
-				<p className='text-4xl text-teal-500'> Blockchain Enthusiast</p>
-				<h2 className='text-4xl text-teal-500 mt-20'>My Socials</h2>
+				<p className='text-4xl text-teal-500 mt-10'>Full Stack Developer, Trader, Artist</p>
+				<h2 className='text-3xl text-teal-500 mt-20'>My Socials</h2>
 				<div className='mt-10'>
-        {links.map((link,index) => (
-          <div className='inline-flex' key={index}>
-            <NavLink name={link.name} href={link.href} />
-          </div>
-         ))} 
-					<p className='text-4xl drop-shadow-2xl text-teal-500 mt-20'>Under Development Stay Tuned!</p>
-					{/* <p className="text-4xl drop-shadow-2xl text-teal-500">Hosted on
-            <a 
-            className="App-link"
-            href="https://vercel.com/dashboard"
-            target="_blank"
-            rel="noopener noreferrer"
-          >- Vercel</a></p> */}
+					{links.map((link, index) => (
+						<div className='inline-flex text-3xl' key={index}>
+							<NavLink name={link.name} href={link.href} />
+						</div>
+					))}
+					<p className='text-3xl drop-shadow-2xl text-teal-500 mt-20'>Under Development Stay Tuned!</p>
 				</div>
 			</header>
 			<Home />
--- src/Navbar.js
+++ src/Navbar.js
@@ -2,7 +2,7 @@ const Navbar = () => {
 	return (
 		<nav className='flex justify-around py-8 mx-auto bg-[#1e1f26]'>
 			<div>
-				<h3 class='text-4xl font-medium text-teal-500 hover:text-gray-500'>
+				<h3 class='text-4xl font-medium text-teal-500 hover:text-gray-500 sm:text-center'>
 					<a href='/'>Owencodes</a>
 				</h3>
 			</div>
@@ -16,6 +16,22 @@ const Navbar = () => {
 				<li className='list-none'>
 					<a className="text-teal-500 hover:text-gray-500 text-4xl" href='#'>Contact</a>
 				</li>
+			</div>
+			<div class="flex lg:hidden">
+				<svg
+				xmlns="http://www.w3.org/2000/svg"
+				class="w-16 h-16"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke="currentColor"
+				>
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="2"
+					d="M4 6h16M4 12h16M4 18h16"
+				/>
+				</svg>
 			</div>
 		</nav>
 	)
