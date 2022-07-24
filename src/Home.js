b325861ece67c53baeea821396b24dc5b1d45b6b
eoghankbdev <eoghankb@gmail.com>
2022-07-24 01:17:54 +0100

    changed tech stack text size

--- src/Home.js
+++ src/Home.js
@@ -3,9 +3,9 @@ function Home() {
 
   return (
     <div className="absolute bottom-0 flex justify-center py-8 mx-auto mt-40 bg-[#1e1f26] w-full">
-            <a className="text-4xl text-teal-500 hover:text-gray-500 font-medium mx-10 cursor-pointer">React</a>
-            <a className="text-4xl text-teal-500 hover:text-gray-500 font-medium mx-10 cursor-pointer">TailwindCSS</a>
-            <a className="text-4xl text-teal-500 hover:text-gray-500 font-medium mx-10 cursor-pointer">GraphQL</a>
+            <a className="text-3xl text-teal-500 hover:text-gray-500 font-medium mx-10 cursor-pointer">React</a>
+            <a className="text-3xl text-teal-500 hover:text-gray-500 font-medium mx-10 cursor-pointer">TailwindCSS</a>
+            <a className="text-3xl text-teal-500 hover:text-gray-500 font-medium mx-10 cursor-pointer">GraphQL</a>
     </div>
   );
 }
