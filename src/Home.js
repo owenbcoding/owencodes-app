15b8677be706a657c7a95b62c03790e83258e1bf
eoghankbdev <eoghankb@gmail.com>
2022-07-25 01:53:34 +0100

    Add copy to the footer of the page

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,14 @@
-
 function Home() {
-
-  return (
-    <div className="absolute bottom-0 flex justify-center py-8 mx-auto mt-40 bg-[#1e1f26] w-full">
-            <a className="text-3xl text-teal-500 hover:text-gray-500 font-medium mx-10 cursor-pointer">React</a>
-            <a className="text-3xl text-teal-500 hover:text-gray-500 font-medium mx-10 cursor-pointer">TailwindCSS</a>
-            <a className="text-3xl text-teal-500 hover:text-gray-500 font-medium mx-10 cursor-pointer">GraphQL</a>
-    </div>
-  );
+	return (
+		<div className='mx-auto absolute bottom-0 py-8 mt-40 bg-[#1e1f26] w-full'>
+				<div className="flex justify-center mx-auto">
+					<a className='text-3xl text-teal-500 hover:text-gray-500 font-medium mx-10 cursor-pointer'>React</a>
+					<a className='text-3xl text-teal-500 hover:text-gray-500 font-medium mx-10 cursor-pointer'>TailwindCSS</a>
+					<a className='text-3xl text-teal-500 hover:text-gray-500 font-medium mx-10 cursor-pointer'>GraphQL</a>
+				</div>
+        <p className="text-teal-500 font-medium mr-10">&copy; Owencodes 2022 | All Rights Reserved</p>
+		</div>
+	)
 }
 
-export default Home;
+export default Home
