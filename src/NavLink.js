b27c1378208317a72dd3dff3ec99688f60b43584
eoghankbdev <eoghankb@gmail.com>
2022-07-30 18:19:14 +0100

    changed text color

--- src/NavLink.js
+++ src/NavLink.js
@@ -2,7 +2,7 @@
 export default function NavLink(props) {
 	return (
 		<>
-		<a className='m-4 text-teal-500 hover:text-gray-500 text-3xl' href={props.href} target='_blank' rel='noopener noreferrer'>
+		<a className='m-4 text-violet-500 hover:text-gray-500 text-3xl' href={props.href} target='_blank' rel='noopener noreferrer'>
 			{props.name}
 		</a>
 		</>
